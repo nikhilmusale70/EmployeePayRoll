e2afe4ba53a050d1ffaad5fd68a370df0636aa67
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-09 10:40:31 +0530

    [Nikhil] - took input from console and then printed on console

--- src/main/java/EmployeePayroll/EmployeePayroll.java
+++ src/main/java/EmployeePayroll/EmployeePayroll.java
@@ -1,5 +1,43 @@
 package EmployeePayroll;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EmployeePayroll {
-    
+    int id;
+    String name;
+    Double salary;
+    ArrayList<EmployeePayrollData> empData = new ArrayList<EmployeePayrollData>();
+    EmployeePayrollData dataObj;
+
+
+    public static void main(String[] args) {
+        EmployeePayroll obj = new EmployeePayroll();
+        obj.readingDataFromConsole();
+        obj.readingDataFromConsole();
+        obj.writingOnConsole();
+
+    }
+
+    public void readingDataFromConsole(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter id :- ");
+        id = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter Name :- ");
+        name = sc.nextLine();
+        System.out.print("Enter salary :- ");
+        salary = sc.nextDouble();
+        dataObj = new EmployeePayrollData(id,name,salary);
+        empData.add(dataObj);
+    }
+
+    public void writingOnConsole(){
+        for (int i=0; i<empData.size(); i++) {
+            System.out.println("Id :-" + empData.get(i).id);
+            System.out.println("Name :-" + empData.get(i).name);
+            System.out.println("Salary:-" + empData.get(i).salary);
+        }
+
+    }
+
 }
--- src/main/java/EmployeePayroll/EmployeePayrollData.java
+++ src/main/java/EmployeePayroll/EmployeePayrollData.java
@@ -5,5 +5,10 @@ public class EmployeePayrollData {
     public String name;
     public double salary;
 
+    EmployeePayrollData(int id, String name, double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 
 }
